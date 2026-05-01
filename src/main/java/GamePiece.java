7d89ab087fd3d2d3a4e7753e7b49d6146b114827
leon-chiang-ics-4u1-02-26 <Leon.Chiang@student.tdsb.on.ca>
2026-05-01 13:21:21 +0000

    Update ReadMe

---
+++ src/main/java/GamePiece.java
@@ -0,0 +1,22 @@
+/**
+ * Class the defines a game piece by name, color, and point value
+ * @author Leon Chiang
+ * @version 1.0.0
+ */
+
+public class GamePiece {
+
+    private String label;
+    private String color;
+    private int pointValue;
+
+    /**
+     * Constructor for GamePiece class
+     * @param label Name of the piece
+     * @param color Color of the piece
+     * @param pointValue Scoring value of the piece
+     * @throws IllegalArgumentException If label or color are null or empty strings,or if point value is less then 0
+     */
+
+    
+}
